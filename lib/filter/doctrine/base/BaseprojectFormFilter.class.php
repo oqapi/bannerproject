1f070a64055d204b7d119beb2e54e514fec13454
roryzweistra <rory@ryuu.nl>
2011-01-26 14:27:55 +0100

    initial install

---
+++ lib/filter/doctrine/base/BaseprojectFormFilter.class.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Project filter form base class.
+ *
+ * @package    sf_sandbox
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BaseProjectFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'project_id' => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'name'       => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+    ));
+
+    $this->setValidators(array(
+      'project_id' => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+      'name'       => new sfValidatorPass(array('required' => false)),
+      'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+    ));
+
+    $this->widgetSchema->setNameFormat('project_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Project';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'         => 'Number',
+      'project_id' => 'Number',
+      'name'       => 'Text',
+      'created_at' => 'Date',
+      'updated_at' => 'Date',
+    );
+  }
+}
