5116189c10ab2ef674ed57bc2c7d7ccc7d51bd0a
Joeri de Bruin <joeri@bannerproject.localdomain>
2011-01-28 22:08:36 +0100

    added banner_position and client_banner

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -8,5 +8,7 @@ class ProjectConfiguration extends sfProjectConfiguration
   public function setup()
   {
     $this->enablePlugins('sfDoctrinePlugin');
+    sfConfig::set('sf_thumbnail_dir', sfConfig::get('sf_upload_dir').'/thumbnails');
+    $this->enablePlugins('sfThumbnailPlugin');
   }
 }
