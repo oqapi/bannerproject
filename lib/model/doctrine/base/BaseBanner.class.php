b2cfef5e704eda069a82892fdd04bf8fcad3b86f
Joeri de Bruin <joeri@bannerproject.localdomain>
2011-02-01 09:53:46 +0100

    added url

--- lib/model/doctrine/base/BaseBanner.class.php
+++ lib/model/doctrine/base/BaseBanner.class.php
@@ -10,6 +10,7 @@ Doctrine_Manager::getInstance()->bindComponent('Banner', 'doctrine');
  * @property integer $project_id
  * @property string $image_url
  * @property string $image_text
+ * @property string $banner_url
  * @property string $text_font
  * @property string $text_color
  * @property integer $font_size
@@ -19,6 +20,7 @@ Doctrine_Manager::getInstance()->bindComponent('Banner', 'doctrine');
  * @method integer             getProjectId()  Returns the current record's "project_id" value
  * @method string              getImageUrl()   Returns the current record's "image_url" value
  * @method string              getImageText()  Returns the current record's "image_text" value
+ * @method string              getBannerUrl()  Returns the current record's "banner_url" value
  * @method string              getTextFont()   Returns the current record's "text_font" value
  * @method string              getTextColor()  Returns the current record's "text_color" value
  * @method integer             getFontSize()   Returns the current record's "font_size" value
@@ -27,6 +29,7 @@ Doctrine_Manager::getInstance()->bindComponent('Banner', 'doctrine');
  * @method Banner              setProjectId()  Sets the current record's "project_id" value
  * @method Banner              setImageUrl()   Sets the current record's "image_url" value
  * @method Banner              setImageText()  Sets the current record's "image_text" value
+ * @method Banner              setBannerUrl()  Sets the current record's "banner_url" value
  * @method Banner              setTextFont()   Sets the current record's "text_font" value
  * @method Banner              setTextColor()  Sets the current record's "text_color" value
  * @method Banner              setFontSize()   Sets the current record's "font_size" value
@@ -62,6 +65,15 @@ abstract class BaseBanner extends sfDoctrineRecord
              'length' => 255,
              ));
         $this->hasColumn('image_text', 'string', 255, array(
+             'type' => 'string',
+             'fixed' => 0,
+             'unsigned' => false,
+             'primary' => false,
+             'notnull' => false,
+             'autoincrement' => false,
+             'length' => 255,
+             ));
+        $this->hasColumn('banner_url', 'string', 255, array(
              'type' => 'string',
              'fixed' => 0,
              'unsigned' => false,
