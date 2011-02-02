fbca2e5ead791c937be05b0c0af52c112a307780
Rory Zweistra <rory@banner-project.oqapi.nl>
2011-02-02 13:41:49 +0000

    removed html tags that belonged to newSuccess.php tempalte

--- apps/frontend/modules/client/templates/_form.php
+++ apps/frontend/modules/client/templates/_form.php
@@ -1,26 +1,17 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
 
-<form action="<?php echo url_for('client/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+<form id="bannergenerator" action="<?php echo url_for('client/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+
 <?php if (!$form->getObject()->isNew()): ?>
 <input type="hidden" name="sf_method" value="put" />
 <?php endif; ?>
-  <table>
-    <tfoot>
-      <tr>
-        <td colspan="2">
-          &nbsp;<a href="<?php echo url_for('client/index') ?>">Back to list</a>
-          <?php if (!$form->getObject()->isNew()): ?>
-            &nbsp;<?php echo link_to('Delete', 'client/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-          <?php endif; ?>
-          <input type="submit" value="Save" />
-        </td>
-      </tr>
-    </tfoot>
-    <tbody>
+
+<fieldset>
       <?php echo $form ?>
-    </tbody>
-  </table>
+</fieldset>
+
+          <input type="submit" value="Generate your banners" />
   <?php foreach ($form->getObject()->getClientBanners() as $clientBanner) { ?>
   <img src="<?php echo $clientBanner->getUrl($form->getObject()->sha1ClientText()); ?>">
   <?php } ?>
