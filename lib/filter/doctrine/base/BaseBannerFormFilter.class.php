34e2376afd4789d6e98bd7b3d9b757b1d04d9d98
roryzweistra <rory@ryuu.nl>
2011-02-04 15:12:46 +0100

    added additional info field

--- lib/filter/doctrine/base/BaseBannerFormFilter.class.php
+++ lib/filter/doctrine/base/BaseBannerFormFilter.class.php
@@ -13,27 +13,29 @@ abstract class BaseBannerFormFilter extends BaseFormFilterDoctrine
   public function setup()
   {
     $this->setWidgets(array(
-      'project_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Project'), 'add_empty' => true)),
-      'image_url'  => new sfWidgetFormFilterInput(),
-      'image_text' => new sfWidgetFormFilterInput(),
-      'banner_url' => new sfWidgetFormFilterInput(),
-      'text_font'  => new sfWidgetFormFilterInput(),
-      'text_color' => new sfWidgetFormFilterInput(),
-      'font_size'  => new sfWidgetFormFilterInput(),
-      'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
-      'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'project_id'      => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Project'), 'add_empty' => true)),
+      'image_url'       => new sfWidgetFormFilterInput(),
+      'image_text'      => new sfWidgetFormFilterInput(),
+      'banner_url'      => new sfWidgetFormFilterInput(),
+      'text_font'       => new sfWidgetFormFilterInput(),
+      'text_color'      => new sfWidgetFormFilterInput(),
+      'font_size'       => new sfWidgetFormFilterInput(),
+      'additional_info' => new sfWidgetFormFilterInput(),
+      'created_at'      => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at'      => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
     ));
 
     $this->setValidators(array(
-      'project_id' => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Project'), 'column' => 'id')),
-      'image_url'  => new sfValidatorPass(array('required' => false)),
-      'image_text' => new sfValidatorPass(array('required' => false)),
-      'banner_url' => new sfValidatorPass(array('required' => false)),
-      'text_font'  => new sfValidatorPass(array('required' => false)),
-      'text_color' => new sfValidatorPass(array('required' => false)),
-      'font_size'  => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
-      'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
-      'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'project_id'      => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Project'), 'column' => 'id')),
+      'image_url'       => new sfValidatorPass(array('required' => false)),
+      'image_text'      => new sfValidatorPass(array('required' => false)),
+      'banner_url'      => new sfValidatorPass(array('required' => false)),
+      'text_font'       => new sfValidatorPass(array('required' => false)),
+      'text_color'      => new sfValidatorPass(array('required' => false)),
+      'font_size'       => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+      'additional_info' => new sfValidatorPass(array('required' => false)),
+      'created_at'      => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at'      => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
     ));
 
     $this->widgetSchema->setNameFormat('banner_filters[%s]');
@@ -53,16 +55,17 @@ abstract class BaseBannerFormFilter extends BaseFormFilterDoctrine
   public function getFields()
   {
     return array(
-      'id'         => 'Number',
-      'project_id' => 'ForeignKey',
-      'image_url'  => 'Text',
-      'image_text' => 'Text',
-      'banner_url' => 'Text',
-      'text_font'  => 'Text',
-      'text_color' => 'Text',
-      'font_size'  => 'Number',
-      'created_at' => 'Date',
-      'updated_at' => 'Date',
+      'id'              => 'Number',
+      'project_id'      => 'ForeignKey',
+      'image_url'       => 'Text',
+      'image_text'      => 'Text',
+      'banner_url'      => 'Text',
+      'text_font'       => 'Text',
+      'text_color'      => 'Text',
+      'font_size'       => 'Number',
+      'additional_info' => 'Text',
+      'created_at'      => 'Date',
+      'updated_at'      => 'Date',
     );
   }
 }
