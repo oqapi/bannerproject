5f0b18425616c57400a1d4a00f51ed0fac61e23a
Rory Zweistra <rory@bannerproject.eu>
2011-02-04 09:08:37 +0000

    removed comments

--- apps/frontend/modules/client/templates/showSuccess.php
+++ apps/frontend/modules/client/templates/showSuccess.php
@@ -49,31 +49,3 @@ target="_blank"><img src="<?php echo $_SERVER['HTTP_HOST'].$clientBanner->getUrl
 </div>
 
 <?php } ?>
-
-<!--<table>
-  <tbody>
-    <tr>
-      <th>Id:</th>
-      <td><?php echo $client->getId() ?></td>
-    </tr>
-    <tr>
-      <th>Project:</th>
-      <td><?php echo $client->getProjectId() ?></td>
-    </tr>
-    <tr>
-      <th>Client text:</th>
-      <td><?php echo $client->getClientText() ?></td>
-    </tr>
-    <tr>
-      <th>Created at:</th>
-      <td><?php echo $client->getCreatedAt() ?></td>
-    </tr>
-    <tr>
-      <th>Updated at:</th>
-      <td><?php echo $client->getUpdatedAt() ?></td>
-    </tr>
-  </tbody>
-</table>
-
-<hr />
--->
